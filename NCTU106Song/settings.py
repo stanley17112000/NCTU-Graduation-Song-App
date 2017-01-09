2dd52b5a835375bad3e2e0f4ea91ac2ddd27513b
stanley <stanley17112000@gmail.com>
2017-01-09 13:54:44 +0800

    log

--- NCTU106Song/settings.py
+++ NCTU106Song/settings.py
@@ -120,28 +120,6 @@ STATICFILES_DIRS =  os.path.join('static'),
 
 STATIC_URL = '/static/'
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+if os.getenv('DATABASE_URL') is not None:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
